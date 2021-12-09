415849aee8fa0b25e491b54e5532eb78e9861700
Vinicius Sá <95035624+Vinicius-Sa@users.noreply.github.com>
2021-12-09 15:22:54 -0300

    Create dumpdb.sh

---
+++ dumpdb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#scrip to dump de db
+  
+if [ $# -eq 0 ]:then
+    echo "Usage $0 <database_name>"
+    exit 255
+    fi
+DBNAME=$1
+mysqldump -u root -p $1 > /var/log/script_database.log
